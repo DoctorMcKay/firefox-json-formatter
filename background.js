26b3c711096bb2b6768b31a03a939acf8164aa3a
Alex Corn <mckay@doctormckay.com>
2019-05-14 02:56:24 -0400

    Added slight delay before showing button to fix issues, and added ability to pretty-print valid JSON without matching content-type header

--- background.js
+++ background.js
@@ -1,24 +1,44 @@
+const DEBUG = false; // controls logging
+
 browser.webRequest.onCompleted.addListener(function(req) {
-	console.log(req.frameId + " frame, type " + req.type + ", tabid " + req.tabId);
+	let prefix = `[TAB ${req.tabId}]`;
+	log(prefix + ` Frame ${req.frameId}, type ${req.type}`);
 	if (req.frameId != 0 || !req.responseHeaders || req.type != "main_frame") {
+		log(prefix + ' Ignoring');
 		return;
 	}
 
 	let isJson = false;
 	req.responseHeaders.forEach(function(header) {
+		log(prefix + ` ${header.name} = ${header.value}`);
 		if (header.name.toLowerCase() == "content-type" && header.value.split(';')[0].trim().toLowerCase() == "application/json") {
 			isJson = true;
 		}
 	});
 
-	if (isJson) {
-		browser.pageAction.show(req.tabId);
-	} else {
-		browser.pageAction.hide(req.tabId);
-	}
+	log(prefix + ' Is JSON: ' + (isJson ? 'true' : 'false'));
+	setTimeout(() => {
+		if (isJson) {
+			browser.pageAction.show(req.tabId);
+		} else {
+			browser.pageAction.hide(req.tabId);
+			browser.tabs.sendMessage(req.tabId, {"type": "checkJson", "tabId": req.tabId}).then((isJson) => {
+				log(prefix + ' Got response to checkJson: ' + isJson);
+				if (isJson) {
+					browser.pageAction.show(req.tabId);
+				}
+			});
+		}
+	}, 100);
 }, {"urls": ["<all_urls>"]}, ["responseHeaders"]);
 
 browser.pageAction.onClicked.addListener(function(tab) {
 	browser.tabs.sendMessage(tab.id, {"type": "format"});
 	browser.pageAction.hide(tab.id);
 });
+
+function log(msg) {
+	if (DEBUG) {
+		console.log(msg);
+	}
+}
--- formatter.js
+++ formatter.js
@@ -1,13 +1,29 @@
-browser.runtime.onMessage.addListener(function(msg) {
-	if (msg.type == "format") {
-		try {
-			let pre = document.createElement('pre');
-			pre.textContent = JSON.stringify(JSON.parse(document.body.textContent), undefined, "\t");
-			let body = document.createElement('body');
-			body.appendChild(pre);
-			document.body = body;
-		} catch (ex) {
-			// whatever
-		}
+browser.runtime.onMessage.addListener(function(msg, sender, respond) {
+	switch (msg.type) {
+		case 'format':
+			try {
+				let pre = document.createElement('pre');
+				pre.textContent = JSON.stringify(parseBody(), undefined, "\t");
+				let body = document.createElement('body');
+				body.appendChild(pre);
+				document.body = body;
+			} catch (ex) {
+				// whatever
+			}
+			break;
+
+		case 'checkJson':
+			// Check if this page appears to be valid JSON
+			let body = parseBody();
+			respond(body !== null && typeof body == 'object');
+			break;
 	}
 });
+
+function parseBody() {
+	try {
+		return JSON.parse(document.body.textContent);
+	} catch (ex) {
+		return null;
+	}
+}
