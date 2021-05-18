6bf791f89ff1b17c054fba76f10c74a0db3dfea1
Alex Corn <alex@alexcorn.com>
2021-05-18 01:41:57 -0400

    Updated for MS Edge

--- background.js
+++ background.js
@@ -1,43 +1,32 @@
-const DEBUG = false; // controls logging
+const DEBUG = true; // controls logging
 
-browser.webRequest.onCompleted.addListener(function(req) {
-	let prefix = `[TAB ${req.tabId}]`;
-	log(prefix + ` Frame ${req.frameId}, type ${req.type}`);
-	if (req.frameId != 0 || !req.responseHeaders || req.type != "main_frame") {
-		log(prefix + ' Ignoring');
+chrome.runtime.onMessage.addListener(function(msg, sender, respond) {
+	respond(); // we don't need to send a response, so just do it now
+	
+	if (!msg || !msg.type || msg.type !== 'contentScriptCheckedJson' || !sender || !sender.tab || !sender.tab.id) {
+		log('Ignoring message');
 		return;
 	}
+	
+	setActionStatus(sender.tab.id, msg.value);
+});
 
-	let isJson = false;
-	req.responseHeaders.forEach(function(header) {
-		log(prefix + ` ${header.name} = ${header.value}`);
-		if (header.name.toLowerCase() == "content-type" && header.value.split(';')[0].trim().toLowerCase() == "application/json") {
-			isJson = true;
-		}
-	});
-
-	log(prefix + ' Is JSON: ' + (isJson ? 'true' : 'false'));
-	setTimeout(() => {
-		if (isJson) {
-			browser.pageAction.show(req.tabId);
-		} else {
-			browser.pageAction.hide(req.tabId);
-			browser.tabs.sendMessage(req.tabId, {"type": "checkJson", "tabId": req.tabId}).then((isJson) => {
-				log(prefix + ' Got response to checkJson: ' + isJson);
-				if (isJson) {
-					browser.pageAction.show(req.tabId);
-				}
-			});
-		}
-	}, 100);
-}, {"urls": ["<all_urls>"]}, ["responseHeaders"]);
-
-browser.pageAction.onClicked.addListener(function(tab) {
-	browser.storage.sync.get('indent_style').then((vals) => {
-		browser.tabs.sendMessage(tab.id, {"type": "format", "indent_style": vals.indent_style});
-		browser.pageAction.hide(tab.id);
+chrome.action.onClicked.addListener(function(tab) {
+	chrome.storage.sync.get('indent_style', (vals) => {
+		chrome.tabs.sendMessage(tab.id, {"type": "format", "indent_style": vals.indent_style});
+		setActionStatus(tab.id, false);
+		chrome.action.setPopup({tabId: tab.id, popup: 'popups/already_formatted.html'});
 	});
 });
+
+function setActionStatus(tabId, enabled) {
+	log('Setting page action for tab ' + tabId + ' to ' + enabled);
+	
+	chrome.action.setPopup({tabId, popup: enabled ? '' : 'popups/no_json.html'});
+	
+	chrome.action.setBadgeBackgroundColor({tabId, color: '#f00'});
+	chrome.action.setBadgeText({tabId, text: enabled ? 'JSON' : ''});
+}
 
 function log(msg) {
 	if (DEBUG) {
--- options_ui/options.js
+++ options_ui/options.js
@@ -1,13 +1,13 @@
 // This is run in a defer tag so the page is loaded when this runs
 
-browser.storage.sync.get('indent_style').then((vals) => {
+chrome.storage.sync.get('indent_style', (vals) => {
 	if (vals.indent_style) {
 		document.getElementById('indent-style').value = vals.indent_style == "\t" ? 'tab' : vals.indent_style;
 	}
 });
 
 document.getElementById('indent-style').addEventListener('change', function() {
-	browser.storage.sync.set({
+	chrome.storage.sync.set({
 		"indent_style": this.value == "tab" ? "\t" : parseInt(this.value, 10)
 	});
 });
